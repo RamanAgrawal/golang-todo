be84d17958d887e59f0271c54c60f275a37619d9
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2025-08-24 20:51:05 +0530

    add welcome route to main application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,6 +10,12 @@ func main() {
 	app := fiber.New()
 
 	api := app.Group("/api")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"msg": "welcome",
+		})
+	})
 	routes.TodoRoutes(api.Group("/todo"))
 
 	app.Listen(":3000")
